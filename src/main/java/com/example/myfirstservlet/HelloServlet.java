980a76588216f8dd39f1367b84465e9b51972eef
Sahilkhan1996 <sahilkhan199655@gmail.com>
2023-02-04 23:28:47 +0530

    Adding the code to validate the user input name

--- src/main/java/com/example/myfirstservlet/HelloServlet.java
+++ src/main/java/com/example/myfirstservlet/HelloServlet.java
@@ -1,26 +1,36 @@
 package com.example.myfirstservlet;
 
 import java.io.*;
+import java.util.regex.Pattern;
 
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
-    private String message;
 
-    public void init() {
-        message = "Servlet page!";
-    }
-
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         response.setContentType("text/html");
+        String username=request.getParameter("user");
+        String password=request.getParameter("pwd");
+         String user="Sahil";
+         String pass="Khan";
+        //if(username.equals(user) && password.equals(pass) ){
+         if(Pattern.matches("^[A-Z][a-z]{2,}$", username)) {
+             request.setAttribute("user", user);
+             request.getRequestDispatcher("Login.jsp").forward(request, response);
+         }
+        //}
+        else{
+            RequestDispatcher rd= getServletContext().getRequestDispatcher("/index.html");
+            PrintWriter out = response.getWriter();
+            out.println("<html><body>");
+            out.println("<h1 style='color:red'>Either Name or password is wrong</h1>");
+            rd.include(request,response);
+        }
 
-        // Hello
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
-        out.println("</body></html>");
     }
 
     public void destroy() {
